14066cd567c4b3a179f40ee227f7dd3b59c0bec9
Philip Lemmerz <plemmerz@yahoo.de>
2023-08-02 11:54:29 +0200

    shop link added

--- pages/karriere.js
+++ pages/karriere.js
@@ -15,32 +15,32 @@ export default function KarrierePage() {
                 <div className={styles.workOfferWrapper}>
                     
                     <div className={styles.workOfferBox}>
-                        <h3> Kollegin Warenauszeichnung / Sortierung in Swisttal gesucht</h3>
+                        <h3>Möbelmonteure / Auslieferungsfahrer (m/w/d) gesucht </h3>
                         <Image className={styles.image} src="/sozialkaufhaus-swisttal-bei-koeln.webp" alt="Sozialkauhaus Bonn Lannesdorf" width={300} height={200}></Image>
                         <p> Arbeitsstelle: Breniger Straße 3, 53913 Swisttal </p>
                         <p>
-                            Wir suchen Unterstützung in der Warenauszeichnung und Verräumung in Swisttal Heimerzheim. Seit über 15 Jahren unterstützen wir mit unserem
+                            Wir suchen Auslieferungsfahrer / Möbelmonteure und Umzugshelfer in Swisttal Heimerzheim. Seit über 15 Jahren unterstützen wir mit unserem
                             Second-Hand Kaufhaus in Swisttal bedürftige Menschen aus der Region mit Kleidung, Haushaltswaren und Möbeln.
-                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt Unterstützung in der Warenauszeichnung und Verräumung.
+                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt.
                             Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche Aufgaben, faire & branchenübliche Gehälter und eine familiäre Arbeitsatmosphäre & hilfsbereite
                             Kollegen.
                         </p>
 
                         <p><b>Ihre Aufgaben bei uns umfassen:</b></p>
                         <ul className={styles.list}>
-                            <li>Sortierung & Auszeichnung unserer Sach- & Kleiderspenden</li>
-                            <li>Präsentation unserer Waren im Verkauf</li>
-                            <li>Verpackung von Schrankinhalten auf Haushaltsauflösugen und Umzügen</li>
+                            <li>Ausliefern & Abholen von Möbeln & Küchen</li>
+                            <li>Durchführung von Umzügen & Haushaltsauflösungen</li>
+                            <li>De- & Montage von Möbeln</li>
                         </ul>
                         <p><b>Was Sie mitbringen sollten:</b></p>
                         <ul className={styles.list}>
-                            <li>Interesse an Second Hand Waren und Kleidung</li>
-                            <li>Spaß an Bewegung</li>
-                            <li>Motivation und freundliches Auftreten</li>
+                            <li>Füherschein: Klasse B (PKW)</li>
+                            <li>Spaß an körperlicher Arbeit</li>
+                            <li>Grundkenntnisse in der deutschen Sprache</li>
                         </ul>
                         <p>
-                            Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbungsunterlagen per Email oder postalisch.
-                            Für Rückfragen steht Ihnen Frau Michalewicz gerne telefonisch unter der 02254-600 480 5 zur Verfügung.
+                            Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbung per Email (info@dsk-nrw.de) oder WhatsApp (0151 - 423 859 89). 
+                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter der 02254-600 480 5 zur Verfügung.
                         </p>
                         <p> Arbeitsstelle: Breniger Straße 3 in 53913 Swisttal</p>
                     </div>
