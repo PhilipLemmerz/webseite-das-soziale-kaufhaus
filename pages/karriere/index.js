719c3f46a620aa05b0932b869d689846d30a3381
PhilipLemmerz <plemmerz@yahoo.de>
2023-08-16 17:58:42 +0200

    lpMonteurSwisttal

--- pages/karriere/index.js
+++ pages/karriere/index.js
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
-import styles from '../styles/karriere.module.css';
+import styles from '../../styles/karriere/karriere.module.css'
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function KarrierePage() {
     return (
@@ -13,36 +15,36 @@ export default function KarrierePage() {
             <section className={styles.section}>
                 <h1 className={styles.headline}> Unsere Stellen-Angebote:</h1>
                 <div className={styles.workOfferWrapper}>
-                    
+
                     <div className={styles.workOfferBox}>
-                        <h3>Möbelmonteure / Auslieferungsfahrer (m/w/d) gesucht </h3>
+                        <h3>Möbelmonteur / Auslieferungsfahrer in Vollzeit gesucht - Quereinsteiger willkommen!</h3>
                         <Image className={styles.image} src="/sozialkaufhaus-swisttal-bei-koeln.webp" alt="Sozialkauhaus Bonn Lannesdorf" width={300} height={200}></Image>
                         <p> Arbeitsstelle: Breniger Straße 3, 53913 Swisttal </p>
                         <p>
-                            Wir suchen Auslieferungsfahrer / Möbelmonteure und Umzugshelfer in Swisttal Heimerzheim. Seit über 15 Jahren unterstützen wir mit unserem
-                            Second-Hand Kaufhaus in Swisttal bedürftige Menschen aus der Region mit Kleidung, Haushaltswaren und Möbeln.
-                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt.
-                            Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche Aufgaben, faire & branchenübliche Gehälter und eine familiäre Arbeitsatmosphäre & hilfsbereite
-                            Kollegen.
+                            Seit über 15 Jahren sind wir eines der führenden Second-Hand Kaufhäuser in Swisttal, Bonn und der Region. 
+                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt Möbelmonteure und Auslieferungsfahrer. 
+                            Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche Aufgaben, ein familiäres und hilfsbereites Team und ein faires Gehalt.
                         </p>
 
                         <p><b>Ihre Aufgaben bei uns umfassen:</b></p>
                         <ul className={styles.list}>
-                            <li>Ausliefern & Abholen von Möbeln & Küchen</li>
+                            <li>Möbelauslieferungen & Abholungen</li>
                             <li>Durchführung von Umzügen & Haushaltsauflösungen</li>
                             <li>De- & Montage von Möbeln</li>
                         </ul>
                         <p><b>Was Sie mitbringen sollten:</b></p>
                         <ul className={styles.list}>
-                            <li>Füherschein: Klasse B (PKW)</li>
+                            <li>Füherschein für LKWs bis 7,5 Tonnen</li>
                             <li>Spaß an körperlicher Arbeit</li>
                             <li>Grundkenntnisse in der deutschen Sprache</li>
+                            <li>Handwerkliches Geschick</li>
                         </ul>
                         <p>
-                            Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbung per Email (info@dsk-nrw.de) oder WhatsApp (0151 - 423 859 89). 
-                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter der 02254-600 480 5 zur Verfügung.
+                            Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbung per Email (info@dsk-nrw.de) oder WhatsApp (0151 - 423 859 89).
+                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter 0160-92 654 321 zur Verfügung.
                         </p>
                         <p> Arbeitsstelle: Breniger Straße 3 in 53913 Swisttal</p>
+                        <Link href="/karriere/moebelmonteur-swisttal"><button className={styles.applyBTN}> jetzt bewerben </button></Link>
                     </div>
                 </div>
             </section>
---
+++ pages/karriere/moebelmonteur-swisttal.js
@@ -0,0 +1,111 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from '../../styles/karriere/moebelmonteur-swisttal.module.css';
+import { BsCheck2Circle } from "react-icons/bs";
+import { useState, useEffect } from "react";
+
+export default function MöbelmonteurSwisttal() {
+
+    const [scrollMenu, setScrollMenu] = useState(false);
+    const [scrolledY, setScrolled] = useState(0);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY)
+        }
+        window.addEventListener("scroll", handleScroll);
+
+        if (scrolledY > 800) {
+            activateScrollMenu()
+        } else {
+            deactivateScrollMenu()
+        }
+    });
+
+
+    function deactivateScrollMenu() {
+        setScrollMenu(false);
+    }
+
+    function activateScrollMenu() {
+        setScrollMenu(true);
+    }
+
+
+
+    return (
+        <Fragment>
+            <Head>
+                <title>Jobs Swisttal - Möbelmonteur & Auslieferungsfahrer gesucht</title>
+                <meta name="description" content="Jobs in Swisttal - Wir suchen dich als Möbelmonteur & Auslieferungsfahrer - Bewirb dich jetzt bei uns" />
+            </Head>
+
+            {scrollMenu && <div className={styles.scrollMenu}>
+                <button className={styles.scrollBTN}> jetzt bewerben</button>
+            </div>}
+
+            <div className={styles.header}>
+                <p className={styles.headerSubheadline}>Ihre neue Herausforderung als</p>
+                <h1 className={styles.headerHeadline}>Möbelmonteur & Auslieferungsfahrer in Swisttal</h1>
+            </div>
+            <section className={styles.ContentWrapper}>
+                <section className={styles.ctaSection}>
+                    <button className={styles.ctaBTN}>jetzt bewerben</button>
+                </section>
+                <section className={styles.aboutUsSection}>
+                    <div className={styles.aboutUsContent}>
+                        <h2 className={styles.h2}>Über uns:</h2>
+                        <p>
+                            Seit über 15 Jahren sind wir eines der führenden Second-Hand Kaufhäuser in Swisttal, Bonn und der Region mit 2 Filialen.
+                            Mit über 20 Kollegen & Kolleginen versorgen wir die Region mit Gebraucht-Möbeln, Haushaltswaren und Kleidung und unterstützen unsere Kunden
+                            bei Haushaltsauflösungen & Umzügen. Der Bedarf an Gebraucht-Möbeln und Haushaltswaren wächst seit Jahren und die Anzahl der
+                            Möbelabholungen & Auslieferungen nimmt stetig zu. Aus diesem Grund suchen wir für unser swisttaler Team neue Möbelmonteure und Auslieferungsfahrer
+                            die unsere Kollegen unterstützen. Auf unserer "Über Uns" Seite finden Sie weitere Informationen zu unserer Firmengeschichte und unsern Kaufhäusern.
+                        </p>
+                    </div>
+                    <Image className={styles.heimerzheimImage} width={600} height={300} src="/sozialkaufhaus-swisttal-bei-koeln.webp"></Image>
+                </section>
+                <section className={styles.benefitSection}>
+                    <div className={styles.benefitBox}>
+                        <h2>Wir bieten Ihnen</h2>
+                        <ul className={styles.benefitList}>
+                            <li><BsCheck2Circle className={styles.checkIcon} /> unbefristeten Arbeitsvertrag</li>
+                            <li className={styles.highlight}> <BsCheck2Circle className={styles.checkIcon} /> 14,50 € Stundenlohn</li>
+                            <li> <BsCheck2Circle className={styles.checkIcon} /> umfangreiche Mitarbeiterrabatte</li>
+                            <li><BsCheck2Circle className={styles.checkIcon} /> hochwertiges Arbeitsmaterial</li>
+                            <li><BsCheck2Circle className={styles.checkIcon} /> familiäres Team & hilfsbereite Kollegen</li>
+                        </ul>
+                    </div>
+                </section>
+                <section className={styles.expectionSection}>
+                    <h2>Was wir von Ihnen erwarten</h2>
+                    <div>
+                        <div className={styles.expectionRow}>
+                            <div className={styles.expectionBox}>
+                                <p>Spaß an körperlicher Arbeit</p>
+                            </div>
+                            <div className={styles.expectionBox}>
+                                <p>gute Deutschkenntisse</p>
+                            </div>
+                        </div>
+                        <div className={styles.expectionRow}>
+                            <div className={styles.expectionBox}>
+                                <p>Führerschein für LKWs bis 7,5 Tonnen</p>
+                            </div>
+                            <div className={styles.expectionBox}>
+                                <p>Erfahrung in der Möbeldemontage & Montage</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className={styles.highlightExpections}>Quereinsteiger sind herzlich willkommen!</p>
+                </section>
+                <section className={styles.ctaSection}>
+                    <button className={styles.ctaBTN}>jetzt bewerben</button>
+                </section>
+            </section>
+        </Fragment>
+
+    )
+}
