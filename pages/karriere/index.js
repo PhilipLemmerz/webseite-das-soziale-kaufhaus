92694d0a9a56c7e2b04f1fcff740c619a104f040
PhilipLemmerz <plemmerz@yahoo.de>
2024-01-06 13:38:29 +0100

    carrerPage change

--- pages/karriere/index.js
+++ pages/karriere/index.js
@@ -16,62 +16,38 @@ export default function KarrierePage() {
                 <h1 className={styles.headline}> Unsere Stellen-Angebote:</h1>
                 <div className={styles.workOfferWrapper}>
                     <div className={styles.workOfferBox}>
-                        <h3>Möbelmonteur / Auslieferungsfahrer in Vollzeit gesucht - Quereinsteiger willkommen!</h3>
+                        <h3>Verkäufer(in) / Kassierer(in) für Swisttal gesucht</h3>
+                        <p><em>Quereinsteiger sind herzlich willkommen</em></p>
                         <Image className={styles.image} src="/sozialkaufhaus-swisttal-bei-koeln.webp" alt="Sozialkauhaus Bonn Lannesdorf" width={300} height={200}></Image>
                         <p> Arbeitsstelle: Breniger Straße 3, 53913 Swisttal </p>
                         <p>
-                            Seit über 15 Jahren sind wir eines der führenden Second-Hand Kaufhäuser in Swisttal, Bonn und der Region. 
-                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt Möbelmonteure und Auslieferungsfahrer. 
-                            Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche Aufgaben, ein familiäres und hilfsbereites Team und ein faires Gehalt.
+                            Seit über 15 Jahren sind wir eines der führenden Second-Hand Kaufhäuser in Swisttal, Bonn und der Region. Unser familiäres und hilfsbereites
+                            Team sucht zum nächstmöglichen Zeitpunkt Unterstützung im Verkauf. Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche
+                            Aufgaben, ein familiäres und hilfsbereites Team, interessante Produkte und ein faires Gehalt.
                         </p>
                         <p><b>Ihre Aufgaben bei uns umfassen:</b></p>
                         <ul className={styles.list}>
-                            <li>Möbelauslieferungen & Abholungen</li>
-                            <li>Durchführung von Umzügen & Haushaltsauflösungen</li>
-                            <li>De- & Montage von Möbeln</li>
+                            <li>Verkauf von second-hand Kleidung & Haushaltswaren.</li>
+                            <li>Entgegennahme von Telefonaten & Kundenanfragen</li>
+                        </ul>
+                        <p><b>Wir bieten Ihnen:</b></p>
+                        <ul className={styles.list}>
+                            <li>einen unbefristeten Arbeitsvertrag</li>
+                            <li>umfangreiche Mitarbeiterrabatte</li>
+                            <li>familiäres Team & hilfsbereite Kollegen</li>
+                            <li>faire & branchenübliche Vergütung.</li>
                         </ul>
                         <p><b>Was Sie mitbringen sollten:</b></p>
                         <ul className={styles.list}>
-                            <li>Füherschein Klasse B (PKW)</li>
-                            <li>Spaß an körperlicher Arbeit</li>
-                            <li>Grundkenntnisse in der deutschen Sprache</li>
-                            <li>Handwerkliches Geschick</li>
+                            <li>Sie haben Spaß an Second-Hand Waren & am Umgang mit Menschen</li>
                         </ul>
                         <p>
                             Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbung per Email (info@dsk-nrw.de) oder WhatsApp (0151 - 423 859 89).
-                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter 0160-92 654 321 zur Verfügung.
-                        </p>
-                      
-                        <Link href="/karriere/moebelmonteur-swisttal"><button className={styles.applyBTN}> jetzt bewerben </button></Link>                        
-                    </div>
-
-                    <div className={styles.workOfferBox}>
-                        <h3>Kommissionierer / Lagermitarbeiter / Warenverräumung (m/w/d)</h3>
-                        <Image className={styles.image} src="/sozialkaufhaus-swisttal-bei-koeln.webp" alt="Sozialkauhaus Bonn Lannesdorf" width={300} height={200}></Image>
-                        <p> Arbeitsstelle: Breniger Straße 3, 53913 Swisttal </p>
-                        <p>
-                            Seit über 15 Jahren sind wir eines der führenden Second-Hand Kaufhäuser in Swisttal, Bonn und der Region. 
-                            Unser familiäres und hilfsbereites Team sucht zum nächstmöglichen Zeitpunkt Unterstützung in der Auspreisung & Warenverräumung. 
-                            Freuen Sie sich auf viele verantwortungsvolle und abwechslungsreiche Aufgaben, ein familiäres und hilfsbereites Team und ein faires Gehalt.
+                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter 0151-11 4444 66 zur Verfügung.
                         </p>
 
-                        <p><b>Ihre Aufgaben bei uns umfassen:</b></p>
-                        <ul className={styles.list}>
-                            <li>Sortieren & Auspreisen von Kleidung & Haushaltswaren (Second-Hand)</li>
-                            <li>Verräumung der Waren im Ladengeschäft</li>
-                            <li>Verpacken & Sortieren von Schrankinhalten auf Haushaltsauflösungen und Umzügen</li>
-                        </ul>
-                        <p><b>Was Sie mitbringen sollten:</b></p>
-                        <ul className={styles.list}>
-                            <li>Füherschein Klasse B (PKW)</li>
-                            <li>Sie haben Spaß an Second-Hand Waren & Kleidung</li>
-                        </ul>
-                        <p>
-                            Wenn wir Ihr Interesse geweckt haben freuen wir uns auf Ihre Bewerbung per Email (info@dsk-nrw.de) oder WhatsApp (0151 - 423 859 89).
-                            Für Rückfragen steht Ihnen Herr Lemmerz gerne telefonisch unter 0160-92 654 321 zur Verfügung.
-                        </p>                    
-                        <Link href="/karriere/bewerbungsformular"><button className={styles.applyBTN}> jetzt bewerben </button></Link>                        
-                    </div>                    
+                        <Link href="/karriere/bewerbungsformular"><button className={styles.applyBTN}> jetzt bewerben </button></Link>
+                    </div>              
                 </div>
             </section>
         </Fragment >
